4b0e017c158078b342559ab5d2db059b2f2727cd
abdelrahmanShabaan <boodyelmasry6@gmail.com>
2024-01-25 01:09:46 +0200

    added select option of table menu

--- table.sh
+++ table.sh
@@ -2,3 +2,12 @@
 
 PS3=$'---------------------------- \nSelect From Table Menu: '
 cd ./.db/$1
+
+select var in "Create table" "List table" "Drop table" "Insert row" "Show data" "Delete row" "Update cell" "Exit"
+do 
+    case $var in
+        "Create table")
+            #read input of table name from user and save in {name}
+            read -p "Please, Enter table Name, Dr.Mina <3 : " name
+
+esac
