71900bff8e30ffe8a358844769100e27e346b513
abdelrahmanShabaan <boodyelmasry6@gmail.com>
2024-01-25 01:01:15 +0200

    make create option with all vaildation and show table menu again

--- db.sh
+++ db.sh
@@ -29,5 +29,32 @@ do
             #We will prompt database name from user
             read -p "Please, enter DB name, Dr.Mina <3 : " name
 
+            #Check vaildation with Regex 
+            if [[ $name == *['!'@#\$%^\&*()-+\.\/]* ]]; then
+                echo 
+                echo "! @ # $ % ^ () + . -  are not allowed, Dr.Mina <3 !"
+                continue
+            fi
+            name=`echo $name | tr " " "_"`
+            #Check vaildation here too with Regex 
+            if [[ ! $name = [0-9]* ]];then
+                if [[ -e ./.db/$name ]];then
+                    echo "Sorry , Dr.Mina <3 ; Databse Already Exist"
+                else 
+                    #If name write with right syntx we need start make directory 
+                    mkdir ./.db/$name 
+                    #we will chmod to give access to modify on database 
+                    chmod u+rwx ./.db/$name
+                    #Print at the end database create successfully
+                    echo "DB created successfully, Dr.Mina <3"
+                fi
+            else
+                echo "Sorry , Dr.Mina <3 ; Invalid name, Please Try Again"
+            fi
+            echo
+            echo "--------------------------------------------------------"
+            echo "1) Create DB   3) Connect DB  5) Exit"
+            echo "2) List DB     4) Remove DB"
+
         ;;
 
